18821dd9516b819518e4a82f37fa0e782f886e09
synt-xerror <169557594+synt-xerror@users.noreply.github.com>
2026-04-21 11:18:19 -0300

    remove embedded plugins - now managed via manyplug

--- src/plugins/obrigado/index.js
+++
@@ -1,8 +0,0 @@
-import { CMD_PREFIX } from "../../config.js";
-const gatilhos = ["obrigado", "valeu", "brigado"];
-
-export default async function ({ msg }) {
-  if (!gatilhos.some(g => msg.is(CMD_PREFIX + g))) return;
-
-  await msg.reply("😊 Por nada!");
-}
