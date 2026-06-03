9fde1caf6345e3e67b7836f2f416d1ffaa042c04
SyntaxError! <me@stxerr.dev>
2026-06-02 22:22:49 -0300

    refactor(logger): add embeed color object

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -1,6 +1,9 @@
-import {
-  c, now,
-} from "./formatter.js";
+const c = {
+  reset: "\x1b[0m", bold: "\x1b[1m", dim: "\x1b[2m",
+  green: "\x1b[32m", yellow: "\x1b[33m", cyan: "\x1b[36m",
+  red: "\x1b[31m", gray: "\x1b[90m", white: "\x1b[37m",
+  blue: "\x1b[34m", magenta: "\x1b[35m",
+};
 
 /**
  * ManyBot central logger.
